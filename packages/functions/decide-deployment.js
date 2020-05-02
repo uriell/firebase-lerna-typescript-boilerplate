fda43de514d10d9c7c313cb64760888375fd6345
Uriell <uriellarttusviana+github@gmail.com>
2020-05-02 18:44:07 -0300

    fetch file changes for each commit

--- packages/functions/decide-deployment.js
+++ packages/functions/decide-deployment.js
@@ -1,19 +1,45 @@
 const ts = require('typescript');
+const fetch = require('node-fetch');
 
-console.log(typeof process.env.GITHUB_TOKEN === 'string');
+const { COMPARE_URL, BEFORE_SHA, AFTER_SHA, GITHUB_TOKEN } = process.env;
+
+const getCompareUrl = (baseUrl, base, head) =>
+  baseUrl
+    .replace('{base}', base.substr(0, 7))
+    .replace('{head}', head.substr(0, 7));
+
+const fetchGithub = (url, authToken, options = {}) =>
+  fetch(url, {
+    headers: {
+      ...(options.headers || {}),
+      Authorization: 'Bearer' + authToken,
+    },
+  }).then((res) => res.json());
+
+async function getFilesChanged() {
+  const compareUrl = getCompareUrl(COMPARE_URL, BEFORE_SHA, AFTER_SHA);
+  const { files } = await fetchGithub(compareUrl, GITHUB_TOKEN);
+
+  return files.map((file) => file.filename);
+}
+
+console.log(getFilesChanged().then(console.log).catch(console.error));
 
 function parse(code = '', filename = 'astExplorer.ts') {
-  const program = ts.createProgram([filename], {}, {
+  const host = {
     fileExists: () => true,
-    getCanonicalFileName: filename => filename,
+    getCanonicalFileName: (filename) => filename,
     getCurrentDirectory: () => '',
     getDefaultLibFileName: () => 'lib.d.ts',
     getNewLine: () => '\n',
-    getSourceFile: filename => ts.createSourceFile(filename, code, ts.ScriptTarget.Latest, true),
+    getSourceFile: (filename) =>
+      ts.createSourceFile(filename, code, ts.ScriptTarget.Latest, true),
     readFile: () => null,
     useCaseSensitiveFileNames: () => true,
     writeFile: () => null,
-  });
-  
+  };
+
+  const program = ts.createProgram([filename], {}, host);
+
   return program.getSourceFile(filename);
 }
