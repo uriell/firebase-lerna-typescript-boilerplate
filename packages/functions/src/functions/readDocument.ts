4fd47819a0f789789aae2c15eb30b4ba6f993174
Uriell <uriellarttusviana+github@gmail.com>
2020-05-02 19:52:58 -0300

    adding document read to fn

--- packages/functions/src/functions/readDocument.ts
+++ packages/functions/src/functions/readDocument.ts
@@ -1,7 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-export default functions.https.onRequest((request, response) => {
-  const documentPath = request.path;
+import { toObject } from '../utils/firestore';
+
+export default functions.https.onRequest(async (request, response) => {
+  const documentPath = request.path.substr(1);
+
+  const document = await admin
+    .firestore()
+    .doc(documentPath)
+    .get()
+    .then(toObject);
 
   return response.status(200).json({
     meta: {
@@ -9,6 +18,7 @@ export default functions.https.onRequest((request, response) => {
     },
     data: {
       documentPath,
+      document,
     },
   });
 });
