a99b1fe20c78d564fd7c0cbb07e9488f538c2d78
Uriell <uriellarttusviana+github@gmail.com>
2020-05-03 13:20:59 -0300

    removing locale example

--- packages/functions/src/functions/helloWorld.ts
+++ packages/functions/src/functions/helloWorld.ts
@@ -1,15 +1,5 @@
 import * as functions from 'firebase-functions';
 
-import { getLocaleStrings } from '../utils/locale';
-
-export default functions.https.onRequest(async (request, response) => {
-  let lang = request.headers['Accept-Language'];
-
-  if (typeof lang !== 'string') {
-    lang = 'en_US';
-  }
-
-  const localeStrings = await getLocaleStrings('hello_world');
-
-  response.send(localeStrings[lang] || localeStrings.en_US);
+export default functions.https.onRequest((request, response) => {
+  response.send('Hello world');
 });
