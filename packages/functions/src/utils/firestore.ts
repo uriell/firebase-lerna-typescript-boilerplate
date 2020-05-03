84b607855237d3dfb375f2f7b30d7efcf78d0cde
Uriell <uriellarttusviana+github@gmail.com>
2020-05-03 13:16:00 -0300

    updating typing on firestore util

--- packages/functions/src/utils/firestore.ts
+++ packages/functions/src/utils/firestore.ts
@@ -9,4 +9,4 @@ export const toObject = <T extends DocumentData>(doc: Document) =>
   ({ id: doc.id, ...doc.data() } as T);
 export const toObjects = <T extends DocumentData>(
   snapshot: firestore.QuerySnapshot
-) => snapshot.docs.map((doc: Document) => toObject<T>(doc)) as T[];
+) => snapshot.docs.map((doc) => toObject<T>(doc)) as T[];
